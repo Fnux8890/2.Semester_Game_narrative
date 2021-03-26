01df78fbe1604f4eda1d1a5c4c8eba65c9a63cd5
Michael Pedersen <micpe18@student.sdu.dk>
2021-03-26 13:22:20 +0100

    Survey added

--- ProjectWebsite/Website/src/js/main.js
+++ ProjectWebsite/Website/src/js/main.js
@@ -9,9 +9,20 @@ $(() => {
     $("#sidebar").toggleClass("active-nav");
     $(".my-container").toggleClass("active-cont");
     $("#menu-btn").css({
-      'transition': '0.4s',
-      "left": state ? '40px' : '190px'
-    })
+      transition: "0.4s",
+      left: state ? "40px" : "190px",
+    });
     state = !state;
   });
+
+  var response;
+  $.ajax({
+    type: "GET",
+    url: "html/Survey.html",
+    async: false,
+    success: function (text) {
+      response = text;
+    },
+  });
+
 });

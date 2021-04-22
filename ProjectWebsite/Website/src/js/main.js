854ce50146a168b2e93fe5727d3695bbebf8c445
Michael Pedersen <micpe18@student.sdu.dk>
2021-04-22 21:23:25 +0200

    plzz work webGL demo0.1

--- ProjectWebsite/Website/src/js/main.js
+++ ProjectWebsite/Website/src/js/main.js
@@ -25,53 +25,5 @@ $(() => {
     },
   });
 
-      var buildUrl = "../../Demo/Demo 0.1/Build";
-      var loaderUrl = buildUrl + "/Unity Builds.loader.js";
-      var config = {
-        dataUrl: buildUrl + "/Unity Builds.data.gz",
-        frameworkUrl: buildUrl + "/Unity Builds.framework.js.gz",
-        codeUrl: buildUrl + "/Unity Builds.wasm.gz",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "DefaultCompany",
-        productName: "The Narrative RPG game",
-        productVersion: "0.1",
-      };
-
-      var container = document.querySelector("#unity-container");
-      var canvas = document.querySelector("#unity-canvas");
-      var loadingBar = document.querySelector("#unity-loading-bar");
-      var progressBarFull = document.querySelector("#unity-progress-bar-full");
-      var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-      var mobileWarning = document.querySelector("#unity-mobile-warning");
-
-      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        container.className = "unity-mobile";
-        config.devicePixelRatio = 1;
-        mobileWarning.style.display = "block";
-        setTimeout(() => {
-          mobileWarning.style.display = "none";
-        }, 5000);
-      } else {
-        canvas.style.width = "960px";
-        canvas.style.height = "600px";
-      }
-      loadingBar.style.display = "block";
-
-      var script = document.createElement("script");
-      script.src = loaderUrl;
-      script.onload = () => {
-        createUnityInstance(canvas, config, (progress) => {
-          progressBarFull.style.width = 100 * progress + "%";
-        }).then((unityInstance) => {
-          loadingBar.style.display = "none";
-          fullscreenButton.onclick = () => {
-            unityInstance.SetFullscreen(1);
-          };
-        }).catch((message) => {
-          alert(message);
-        });
-      };
-      document.body.appendChild(script);
-
 });
 

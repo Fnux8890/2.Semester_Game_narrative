ffc11d13cc9a8ac9bc0d57c85fc4de755dba5c7e
Michael Pedersen <micpe18@student.sdu.dk>
2021-05-16 18:14:37 +0200

    some GUI Dialogue stuff

--- The Narrative RPG game/Assets/Scripts/GameSystems/Dialogue/DialogueUIManager.cs
+++ The Narrative RPG game/Assets/Scripts/GameSystems/Dialogue/DialogueUIManager.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using GameSystems.CustomEventSystems.Interaction;
 using GameSystems.Dialogue.Dialogue_Json_Classes;
 using PlayerControl;
@@ -29,21 +31,18 @@ namespace GameSystems.Dialogue
 
         private void BranchedDialogueOrNot(Node currentNode, bool hasChoices)
         {
-            HandleBox(
-                currentNode: currentNode,
-                isBox: currentNode.is_box,
-                hasChoices: hasChoices);
+           StartCoroutine(HandleBox(currentNode,currentNode.is_box,hasChoices));
         }
 
-        private void HandleBox(Node currentNode, bool isBox, bool hasChoices)
+        private IEnumerator HandleBox(Node currentNode, bool isBox, bool hasChoices)
         {
             if (isBox)
             {
                 var dialogueBox = _dialogueBoxes.Find(box => box.name == "DialogueBox Top");
                 dialogueBox.gameObject.SetActive(true);
                 HasDecision(currentNode, hasChoices, dialogueBox, isBox);
-                var dialogue = dialogueBox.transform.Find("Dialogue").GetComponent<Text>();
-                dialogue.text = currentNode.Text;
+                var dialogue = dialogueBox.transform.Find("Dialogue").GetChild(0).GetComponent<Text>();
+                yield return StartCoroutine(TypeWriter(currentNode.Text, dialogue));
             }
 
             if (!isBox)
@@ -52,23 +51,28 @@ namespace GameSystems.Dialogue
                 if (currentNode.character == "Player")
                 {
                     dialogueBox = _dialogueBoxes.Find(box => box.name == "DialogueBoxLeft");
-                    dialogueBox.transform.Find("Name").GetComponent<Text>().text = currentNode.character;
+                    dialogueBox.transform.Find("Name").GetChild(0).GetComponent<Text>().text = currentNode.character;
                 }
                 else
                 {
                     dialogueBox = _dialogueBoxes.Find(box => box.name == "DialogueBoxRight");
-                    dialogueBox.transform.Find("Name").GetComponent<Text>().text = currentNode.character;
+                    dialogueBox.transform.Find("Name").GetChild(0).GetComponent<Text>().text = currentNode.character;
                 }
                 dialogueBox.gameObject.SetActive(true);
                 HasDecision(currentNode,hasChoices, dialogueBox, isBox);
-                var dialogue = dialogueBox.transform.Find("Dialogue").GetComponent<Text>();
-                dialogue.text = currentNode.Text;
+                var dialogue = dialogueBox.transform.Find("Dialogue").GetChild(0).GetComponent<Text>();
+                yield return StartCoroutine(TypeWriter(currentNode.Text, dialogue));
             }
             
         }
 
         private void HasDecision(Node currentNode,bool hasChoices, GameObject dialogueBox, bool isBox)
         {
+            if (dialogueBox.transform.Find("Decision Panel") == true)
+            {
+                dialogueBox = dialogueBox.transform.Find("Decision Panel").gameObject;
+                dialogueBox.gameObject.SetActive(false);
+            }
             if (dialogueBox.transform.Find("Decide") == true)
             {
                 
@@ -76,10 +80,14 @@ namespace GameSystems.Dialogue
                 decision.gameObject.SetActive(hasChoices);
                 if (hasChoices)
                 {
+                    dialogueBox.gameObject.SetActive(true);
                     for (var i = 0; i < decision.childCount; i++)
-                    {
+                    { 
                         var currentChild = decision.GetChild(i);
-                        currentChild.GetComponentInChildren<Text>().text = currentNode.choices[i].Text;
+                        var textButtonText = currentChild.GetComponentInChildren<Text>();
+                        textButtonText.text = currentNode.choices[i].Text ;
+                        var rect = currentChild.GetComponent<RectTransform>().rect;
+                        rect.width = textButtonText.gameObject.GetComponent<RectTransform>().rect.width;
                         currentChild.GetComponent<Button>().onClick.AddListener(delegate { Choice(currentChild.gameObject, currentNode, isBox, dialogueBox); });
                     }
                     PlayerActionControlsManager.Instance.PlayerControls.Land.Interact.Disable();
@@ -100,6 +108,7 @@ namespace GameSystems.Dialogue
                     NewMethod(1,currentChild, currentNode, isBox, currentParent);
                     break;
             }
+            
         }
 
         private void NewMethod(int choice,GameObject currentChild, Node currentNode, bool isBox, GameObject currentParent)
@@ -107,7 +116,7 @@ namespace GameSystems.Dialogue
             var nextNode = DialogueManager.Instance.Nodes.Find(node => node.node_name == currentNode.choices[choice].next);
             if (isBox)
             {
-                currentParent.SetActive(false);
+                currentParent.transform.parent.gameObject.SetActive(false);
             }
             else
             {
@@ -115,16 +124,31 @@ namespace GameSystems.Dialogue
             }
             InteractionHandler.Instance.OnUpdateNode(nextNode);
             PlayerActionControlsManager.Instance.PlayerControls.Land.Interact.Enable();
+            
         }
 
         private void CloseDialogue()
         {
             foreach (var box in _dialogueBoxes.Where(box => box.activeSelf))
             {
-                box.transform.Find("Dialogue").GetComponent<Text>().text = string.Empty;
+                box.transform.Find("Dialogue").GetChild(0).GetComponent<Text>().text = string.Empty;
                 box.SetActive(false);
             }
         }
+
+        private IEnumerator TypeWriter(string text, Text textToSet)
+        {
+            var sb = new StringBuilder();
+            textToSet.text = sb.ToString();
+            foreach (var ch in text.ToCharArray())
+            {
+                textToSet.text += ch;
+                yield return new WaitForSeconds(1f/30);
+            }
+
+            yield return new WaitForSeconds(4f);
+        }
+        
         
     }
 }
